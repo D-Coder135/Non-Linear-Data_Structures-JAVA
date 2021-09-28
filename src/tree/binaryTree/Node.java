a10af7e0d5f21f8469dc04105a6d87f45583616b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-28 21:09:06 +0530

    add a class Node to the package tree/binaryTree

---
+++ src/tree/binaryTree/Node.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 28/09/21
+ *   Time: 9:08 PM
+ *   File: Node.java
+ */
+
+package tree.binaryTree;
+
+public class Node {
+}
