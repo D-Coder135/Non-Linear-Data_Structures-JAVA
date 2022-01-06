1d9ab6d33ffb4cc9823b90d1851663b4f156de0b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-06 15:37:56 +0530

    extends the BinaryTree.java class inside the BinarySearchTree.java

--- src/trees/binarySearchTree/implementation/BinarySearchTree.java
+++ src/trees/binarySearchTree/implementation/BinarySearchTree.java
@@ -1,30 +1,8 @@
 package trees.binarySearchTree.implementation;
 
 import trees.SearchTree;
+import trees.binaryTree.implementation.BinaryTree;
 
-public class BinarySearchTree<E extends Comparable<E>> implements SearchTree<E> {
-    @Override
-    public boolean add(E item) {
-        return false;
-    }
+public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> implements SearchTree<E> {
 
-    @Override
-    public boolean contains(E target) {
-        return false;
-    }
-
-    @Override
-    public E find(E target) {
-        return null;
-    }
-
-    @Override
-    public E delete(E target) {
-        return null;
-    }
-
-    @Override
-    public boolean remove(E target) {
-        return false;
-    }
 }
