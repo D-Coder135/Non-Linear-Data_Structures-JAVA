7182bb213010931826dc252bf2cab9af6585cd45
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-06 19:35:45 +0530

    add a return statement

--- src/trees/binarySearchTree/implementation/BinarySearchTree.java
+++ src/trees/binarySearchTree/implementation/BinarySearchTree.java
@@ -71,6 +71,7 @@ public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> imp
         if (localRoot == null) {
             // item is equal to localRoot.data
             addReturn = true;
+            return new Node<>(item);
         }
     }
 
