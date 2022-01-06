a921d8d6583e90bc8d67deafb7bed65f89fe4bf1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-06 23:40:54 +0530

    return the localRoot

--- src/trees/binarySearchTree/implementation/BinarySearchTree.java
+++ src/trees/binarySearchTree/implementation/BinarySearchTree.java
@@ -162,6 +162,7 @@ public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> imp
                 } else {
                     // Search for the inorder predecessor and replace deleted node's data with inorder predecessor.
                     localRoot.data = findLargestChild(localRoot.left);
+                    return localRoot;
                 }
             }
         }
