a04d04146c08a9de761b57dad02e83ee481235f2
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-06 15:29:24 +0530

    extends the BinaryTree<E> class inside the BinarySearchTree.java

--- src/tree/binarySearchTree/implementation/BinarySearchTree.java
+++ src/tree/binarySearchTree/implementation/BinarySearchTree.java
@@ -1,4 +1,6 @@
 package tree.binarySearchTree.implementation;
 
-public class BinarySearchTree<E extends Comparable<E>> {
+import com.sun.source.tree.BinaryTree;
+
+public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> {
 }
