aebd192e7e9a8f4b77600f9ebca2738d6340f99a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-06 23:29:51 +0530

    return the localRoot field

--- src/trees/binarySearchTree/implementation/BinarySearchTree.java
+++ src/trees/binarySearchTree/implementation/BinarySearchTree.java
@@ -139,6 +139,7 @@ public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> imp
         } else if (compResult > 0) {
             // item is larger than localRoot.data
             localRoot.right = delete(localRoot.right, item, deleteReturn);
+            return localRoot;
         }
         return null;
     }
