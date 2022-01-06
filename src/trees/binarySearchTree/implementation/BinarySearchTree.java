2f47b9b13a1007c11f47c057f8fe64804e994267
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-06 15:38:11 +0530

    implement the required methods

--- src/trees/binarySearchTree/implementation/BinarySearchTree.java
+++ src/trees/binarySearchTree/implementation/BinarySearchTree.java
@@ -5,4 +5,28 @@ import trees.binaryTree.implementation.BinaryTree;
 
 public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> implements SearchTree<E> {
 
+    @Override
+    public boolean add(E item) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(E target) {
+        return false;
+    }
+
+    @Override
+    public E find(E target) {
+        return null;
+    }
+
+    @Override
+    public E delete(E target) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(E target) {
+        return false;
+    }
 }
