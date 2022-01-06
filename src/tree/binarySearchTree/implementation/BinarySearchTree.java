ff01c1107567d91162f8693ae24cf08a9d777da4
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-06 15:30:42 +0530

    implements the SearchTree<E> interface inside the BinarySearchTree.java

--- src/tree/binarySearchTree/implementation/BinarySearchTree.java
+++ src/tree/binarySearchTree/implementation/BinarySearchTree.java
@@ -1,6 +1,7 @@
 package tree.binarySearchTree.implementation;
 
 import com.sun.source.tree.BinaryTree;
+import tree.SearchTree;
 
-public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> {
+public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> implements SearchTree<E> {
 }
