180e5acb951aa91cab252ce4437587ea1375ca6f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-07 12:59:31 +0530

    extends the BinaryTree.Node<E> class

--- src/trees/avlTree/avlNode/AVLNode.java
+++ src/trees/avlTree/avlNode/AVLNode.java
@@ -1,9 +1,11 @@
 package trees.avlTree.avlNode;
 
+import trees.binaryTree.implementation.BinaryTree;
+
 /**
  * Class to represent an AVL Node. It extends the
  * BinaryTree.Node by adding the balance field.
  */
 
-public class AVLNode<E> {
+public class AVLNode<E> extends BinaryTree.Node<E> {
 }
