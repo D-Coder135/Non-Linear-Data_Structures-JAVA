938a9dec14ab4fffb85cade2387ed95bb2599117
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-07 13:27:32 +0530

    override the add() method

--- src/trees/avlTree/implementation/AVLTree.java
+++ src/trees/avlTree/implementation/AVLTree.java
@@ -4,11 +4,17 @@ import trees.avlTree.BinarySearchTreeWithRotate;
 
 public class AVLTree<E extends Comparable<E>> extends BinarySearchTreeWithRotate<E> {
 
-    /** add starter method.
-     @pre the item to insert implements the Comparable interface.
-     @param item The item being inserted.
-     @return true if the object is inserted; false
-     if the object already exists in the tree
-     @throws ClassCastException if item is not Comparable
+    /**
+     * add starter method.
+     *
+     * @param item The item being inserted.
+     * @return true if the object is inserted; false
+     * if the object already exists in the tree
+     * @throws ClassCastException if item is not Comparable
+     * @pre the item to insert implements the Comparable interface.
      */
+    @Override
+    public boolean add(E item) {
+        return super.add(item);
+    }
 }
