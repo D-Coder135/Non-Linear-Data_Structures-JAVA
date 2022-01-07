51d86d30f481add22f70fd3f3b6be61992815f96
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-07 12:44:41 +0530

    add some comment as class description

--- src/trees/avlTree/BinarySearchTreeWithRotate.java
+++ src/trees/avlTree/BinarySearchTreeWithRotate.java
@@ -1,4 +1,11 @@
 package trees.avlTree;
 
+/**
+ * This class extends the BinarySearchTree by adding the rotate operations.
+ * Rotation will change the balance of a search tree while preserving the
+ * search tree property.
+ * Used as a common base class for self‐balancing trees.
+ */
+
 public class BinarySearchTreeWithRotate {
 }
